2f6bcec3aec561769726eceb449101e984eb1009
jmmaranan <jmmaranan@gmail.com>
2023-05-31 21:42:54 -0400

    Make quick-settings optional

--- indicator.js
+++ indicator.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 const { GObject, Gio } = imports.gi;
 
@@ -11,92 +11,104 @@ const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 const Logger = Me.imports.logger;
 
-const iconName = 'view-grid-symbolic';
+const iconName = "view-grid-symbolic";
 
 const SettingsPopupSwitch = GObject.registerClass(
-class SettingsPopupSwitch extends PopupMenu.PopupSwitchMenuItem {
-  _init(title, settings, bind) {
-    this._settings = settings;
-    const active = !!this._settings.get_boolean(bind);
-    super._init(title, active);
-    Logger.info(bind, active);
-    this.connect('toggled', item => this._settings.set_boolean(bind, item.state));
-  }
-});
-
-const FeatureMenuToggle = GObject.registerClass(
-class FeatureMenuToggle extends QuickSettings.QuickMenuToggle {
-  _init(settings, extWm) {
-    const title = _('Tiling');
-    super._init({ title, iconName, toggleMode: true });
-
-    this._settings = settings;
-    this._extWm = extWm;
-    this._settings.bind('tiling-mode-enabled', this, 'checked', Gio.SettingsBindFlags.DEFAULT);
-
-    this.menu.setHeader(iconName, _('Forge'), _('Tiling Window Management'));
-
-    this.menu.addMenuItem(this._singleSwitch = new SettingsPopupSwitch(
-      _('Gaps Hidden when Single'),
-      this._settings,
-      'window-gap-hidden-on-single'
-    ));
-
-    this.menu.addMenuItem(this._focusHintSwitch = new SettingsPopupSwitch(
-      _('Show Focus Hint Border'),
-      this._settings,
-      'focus-border-toggle'
-    ));
-
-    // Add an entry-point for more settings
-    this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-    const settingsItem = this.menu.addAction(_('Settings'), () => ExtensionUtils.openPrefs());
-
-    // Ensure the settings are unavailable when the screen is locked
-    settingsItem.visible = Main.sessionMode.allowSettings;
-    this.menu._settingsActions[Me.uuid] = settingsItem;
-  }
-});
-
-var FeatureIndicator = GObject.registerClass(
-class FeatureIndicator extends QuickSettings.SystemIndicator {
-  _init(settings, extWm) {
-    super._init();
-
-    // Create the icon for the indicator
-    this._indicator = this._addIndicator();
-    this._indicator.icon_name = iconName;
-
-    // Showing the indicator when the feature is enabled
-    this._settings = settings;
-
-    this._settings.bind(
-      'tiling-mode-enabled',
-      this._indicator,
-      'visible',
-      Gio.SettingsBindFlags.DEFAULT,
-    );
-
-    // Create the toggle and associate it with the indicator, being sure to
-    // destroy it along with the indicator
-    this.quickSettingsItems.push(new FeatureMenuToggle(settings, extWm));
-
-    this.connect('destroy', () => {
-      this.quickSettingsItems.forEach(item => item.destroy());
-    });
-
-    // Add the indicator to the panel and the toggle to the menu
-    QuickSettingsMenu._indicators.add_child(this);
-    this._addItems(this.quickSettingsItems);
-  }
-
-  // To add your toggle above another item, such as Background Apps, add it
-  // using the built-in function, then move them afterwards.
-  _addItems(items) {
-    QuickSettingsMenu._addItems(items);
-
-    for (const item of items) {
-      QuickSettingsMenu.menu._grid.set_child_below_sibling(item, QuickSettingsMenu._backgroundApps.quickSettingsItems[0]);
+  class SettingsPopupSwitch extends PopupMenu.PopupSwitchMenuItem {
+    _init(title, settings, bind) {
+      this._settings = settings;
+      const active = !!this._settings.get_boolean(bind);
+      super._init(title, active);
+      Logger.info(bind, active);
+      this.connect("toggled", (item) => this._settings.set_boolean(bind, item.state));
     }
   }
-});
+);
+
+const FeatureMenuToggle = GObject.registerClass(
+  class FeatureMenuToggle extends QuickSettings.QuickMenuToggle {
+    _init(settings, extWm) {
+      const title = _("Tiling");
+      super._init({ title, iconName, toggleMode: true });
+
+      this._settings = settings;
+      this._extWm = extWm;
+      this._settings.bind("tiling-mode-enabled", this, "checked", Gio.SettingsBindFlags.DEFAULT);
+      this._settings.bind("quick-settings-enabled", this, "visible", Gio.SettingsBindFlags.DEFAULT);
+
+      this.menu.setHeader(iconName, _("Forge"), _("Tiling Window Management"));
+
+      this.menu.addMenuItem(
+        (this._singleSwitch = new SettingsPopupSwitch(
+          _("Gaps Hidden when Single"),
+          this._settings,
+          "window-gap-hidden-on-single"
+        ))
+      );
+
+      this.menu.addMenuItem(
+        (this._focusHintSwitch = new SettingsPopupSwitch(
+          _("Show Focus Hint Border"),
+          this._settings,
+          "focus-border-toggle"
+        ))
+      );
+
+      // Add an entry-point for more settings
+      this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+      const settingsItem = this.menu.addAction(_("Settings"), () => ExtensionUtils.openPrefs());
+
+      // Ensure the settings are unavailable when the screen is locked
+      settingsItem.visible = Main.sessionMode.allowSettings;
+      this.menu._settingsActions[Me.uuid] = settingsItem;
+    }
+  }
+);
+
+var FeatureIndicator = GObject.registerClass(
+  class FeatureIndicator extends QuickSettings.SystemIndicator {
+    _init(settings, extWm) {
+      super._init();
+
+      // Create the icon for the indicator
+      this._indicator = this._addIndicator();
+      this._indicator.icon_name = iconName;
+
+      // Showing the indicator when the feature is enabled
+      this._settings = settings;
+
+      this._settings.connect("changed", (_, name) => {
+        switch (name) {
+          case "tiling-mode-enabled":
+          case "quick-settings-enabled":
+            this._indicator.visible = this._settings.get_boolean(name);
+        }
+      });
+
+      // Create the toggle and associate it with the indicator, being sure to
+      // destroy it along with the indicator
+      this.quickSettingsItems.push(new FeatureMenuToggle(settings, extWm));
+
+      this.connect("destroy", () => {
+        this.quickSettingsItems.forEach((item) => item.destroy());
+      });
+
+      // Add the indicator to the panel and the toggle to the menu
+      QuickSettingsMenu._indicators.add_child(this);
+      this._addItems(this.quickSettingsItems);
+    }
+
+    // To add your toggle above another item, such as Background Apps, add it
+    // using the built-in function, then move them afterwards.
+    _addItems(items) {
+      QuickSettingsMenu._addItems(items);
+
+      for (const item of items) {
+        QuickSettingsMenu.menu._grid.set_child_below_sibling(
+          item,
+          QuickSettingsMenu._backgroundApps.quickSettingsItems[0]
+        );
+      }
+    }
+  }
+);
--- preferences/settings.js
+++ preferences/settings.js
@@ -17,81 +17,93 @@ const { DropDownRow, SwitchRow, PreferencesPage } = Me.imports.widgets;
 const { makeAboutButton } = Me.imports.preferences.about;
 const { production } = Me.imports.settings;
 
-var SettingsPage = GObject.registerClass(class SettingsPage extends PreferencesPage {
-  _init({ settings, window }) {
-    super._init({ title: _("Settings"), icon_name: 'settings-symbolic' });
-    this.add_group({
-      title: _('Settings'),
-      description: _('Toggle Forge\'s high-level features'),
-      header_suffix: makeAboutButton(window),
-      children: [
-        new SwitchRow({
-          title: _('Stacked Tiling Mode'),
-          subtitle: _('Stack windows on top of each other while still being tiled'),
-          experimental: true,
-          settings,
-          bind: 'stacked-tiling-mode-enabled',
-        }),
-        new SwitchRow({
-          title: _('Tabbed Tiling Mode'),
-          subtitle: _('Group tiles windows as tabs'),
-          experimental: true,
-          settings,
-          bind: 'tabbed-tiling-mode-enabled',
-        }),
-      ],
-    })
+var SettingsPage = GObject.registerClass(
+  class SettingsPage extends PreferencesPage {
+    _init({ settings, window }) {
+      super._init({ title: _("Settings"), icon_name: "settings-symbolic" });
+      this.add_group({
+        title: _("Settings"),
+        description: _("Toggle Forge's high-level features"),
+        header_suffix: makeAboutButton(window),
+        children: [
+          new SwitchRow({
+            title: _("Stacked Tiling Mode"),
+            subtitle: _("Stack windows on top of each other while still being tiled"),
+            experimental: true,
+            settings,
+            bind: "stacked-tiling-mode-enabled",
+          }),
+          new SwitchRow({
+            title: _("Tabbed Tiling Mode"),
+            subtitle: _("Group tiles windows as tabs"),
+            experimental: true,
+            settings,
+            bind: "tabbed-tiling-mode-enabled",
+          }),
+        ],
+      });
 
-    this.add_group({
-      title: _('Tiling'),
-      children: [
-        new SwitchRow({
-          title: Msgs.prefs_experimental_preview_hint,
-          experimental: true,
-          settings,
-          bind: 'preview-hint-enabled',
-        }),
-        new SwitchRow({
-          title: _('Show Focus Hint Border'),
-          subtitle: _('Display a colored border around the focused window'),
-          settings,
-          bind: 'focus-border-toggle',
-        }),
-        new DropDownRow({
-          title: Msgs.prefs_appearance_layout_dnd_default_layout,
-          settings,
-          bind: 'dnd-center-layout',
-          items: [
-            { id: 'tabbed', name: _('Tabbed') },
-            { id: 'stacked', name: _('Stacked') },
+      this.add_group({
+        title: _("Tiling"),
+        children: [
+          new SwitchRow({
+            title: Msgs.prefs_experimental_preview_hint,
+            experimental: true,
+            settings,
+            bind: "preview-hint-enabled",
+          }),
+          new SwitchRow({
+            title: _("Show Focus Hint Border"),
+            subtitle: _("Display a colored border around the focused window"),
+            settings,
+            bind: "focus-border-toggle",
+          }),
+          new DropDownRow({
+            title: Msgs.prefs_appearance_layout_dnd_default_layout,
+            settings,
+            bind: "dnd-center-layout",
+            items: [
+              { id: "tabbed", name: _("Tabbed") },
+              { id: "stacked", name: _("Stacked") },
+            ],
+          }),
+          new SwitchRow({
+            title: _("Auto Split"),
+            subtitle: _("Quarter Tiling"),
+            experimental: true,
+            settings,
+            bind: "auto-split-enabled",
+          }),
+          new SwitchRow({
+            title: _("Float Mode Always On Top"),
+            subtitle: _("Floating windows always above tiling windows"),
+            experimental: true,
+            settings,
+            bind: "float-always-on-top-enabled",
+          }),
+          new SwitchRow({
+            title: _("Show Tiling Quick Settings"),
+            subtitle: _("Toggle showing Forge on quick settings"),
+            experimental: true,
+            settings,
+            bind: "quick-settings-enabled",
+          }),
+        ],
+      });
+
+      if (!production) {
+        this.add_group({
+          title: _("Logger"),
+          children: [
+            new DropDownRow({
+              title: _("Logger Level"),
+              settings,
+              bind: "log-level",
+              items: Object.entries(Logger.LOG_LEVELS).map(([name, id]) => ({ id, name })),
+            }),
           ],
-        }),
-        new SwitchRow({
-          title: _('Auto Split'),
-          subtitle: _('Quarter Tiling'),
-          experimental: true,
-          settings,
-          bind: 'auto-split-enabled',
-        }),
-        new SwitchRow({
-          title: _('Float Mode Always On Top'),
-          subtitle: _('Floating windows always above tiling windows'),
-          experimental: true,
-          settings,
-          bind: 'float-always-on-top-enabled',
-        }),
-      ],
-    });
-
-    if (!production) {
-      this.add_group({ title: _('Logger'), children: [new DropDownRow({
-        title: _('Logger Level'),
-        settings,
-        bind: 'log-level',
-        items: Object.entries(Logger.LOG_LEVELS).map(([name, id]) => ({ id, name })),
-      })] });
+        });
+      }
     }
   }
-});
-
-
+);
