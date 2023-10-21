d231533800ac994c5c70b03e09efcf45a1639341
jmmaranan <jmmaranan@gmail.com>
2023-10-21 17:29:32 -0400

    Remove indicator on disable

--- extension.js
+++ extension.js
@@ -61,6 +61,7 @@ export default class ForgeExtension extends Extension {
       this._sessionId = null;
     }
 
+    this._removeIndicator();
     this.extWm?.disable();
     this.keybindings?.disable();
     this.keybindings = null;
